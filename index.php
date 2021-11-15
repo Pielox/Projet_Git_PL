8e7864662cbbc529ee3e0eec7faae6c38bd1f2b9
Pielox <Pielo_@outlook.fr>
2021-11-15 12:23:45 +0100

    initialisation du projet + première version

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Nanum+Gothic+Coding&family=Press+Start+2P&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&family=Press+Start+2P&display=swap" rel="stylesheet">
+
+    <title>Projet_Git_PL</title>
+
+    <style>
+        body{
+            font-family: 'Lato', sans-serif
+            
+            
+        }
+        h1 {
+            font-family: 'Nanum Gothic Coding', monospace;
+            font-family: 'Press Start 2P', cursive;
+        }
+    </style>
+  </head>
+  <body>
+    <h1 class="text-center">Projet_Git</h1>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    
+
+  </body>
+</html>
